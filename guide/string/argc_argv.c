ea0c2b44bca44ad1dd948160a6e23243b1d7bb3c
cesar <cesar.marsfall@gmail.com>
2025-11-14 15:57:58 -0800

    15:57

--- guide/string/argc_argv.c
+++ guide/string/argc_argv.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main(int num, char *letter[]) {
 
-	printf("Number of argc strings: %d\n", argc); // number of strings on the command line
+	printf("Number of argc strings: %d\n", num); // number of strings on the command line
 
-	for(int i = 0 ; i <= argc; i++) {
+	for(int i = 0 ; i <= num; i++) {
 		// argv is a pointer variable that points to each element in the array of strings
 		// array -> ./a.out Hello -> ["./a.out","Hello", null], NULL tells us when the arrays is finished
-		printf("Argc number %d: %s\n", i, argv[i]);  
+		printf("Argc number %d: %s\n", i, letter[i]);  
 	}	
 	return 0;
 
